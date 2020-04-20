5b5e31089783783dc3d417dee9d8a557317b9907
liouoth <mwqyykl@gmail.com>
2020-04-20 18:16:41 +0800

    js

--- src/main/resources/static/js/req.js
+++ src/main/resources/static/js/req.js
@@ -1,3 +1,18 @@
-function all() {
-
+let CONSOLE_REQUEST = {
+    options: {
+        method: 'post',
+        url: '',
+        isAsync: true,
+        content_type: 'application/x-www-form-urlencoded',
+        body: ''
+    },
+    create: function (options) {
+        if (options != null || options.url != '') {
+            this.options = options;
+        }
+        let request = new XMLHttpRequest();
+        return request;
+    }
 }
+
+
